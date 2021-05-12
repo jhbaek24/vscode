f2c9c117b664e97272f4d0753877db657e2b8494
jhbaek24 <jhbaek24@naver.com>
2021-05-12 16:35:52 +0900

    0512

---
+++ js/joinform.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>회원가입</h2>
+    아이디:
+        <input type="text" id="id" placeholder="6~10자리, 소문자, 숫자포함" onblur="idCheck()">
+        <span id="id-check-result"></span><br>
+        
+</body>
+</html>
